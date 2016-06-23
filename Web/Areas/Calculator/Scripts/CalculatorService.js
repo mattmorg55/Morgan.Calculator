96e6ab9e86404de9f6a27eb4f1c5633922af6461
Matt <mattmorg55@gmail.com>
2016-06-23 09:50:21 -0400

    Changes

--- Web/Areas/Calculator/Scripts/CalculatorService.js
+++ Web/Areas/Calculator/Scripts/CalculatorService.js
@@ -1,13 +1,33 @@
 ﻿(function (app) {
 
+	
 	var calculatorService = function ($http, calculatorApiUrl) {
 
+		var _exec = function (operation, left, right) {
+			return $http.get($.validator.format("{0}/{1}/{2}/{3}/", calculatorApiUrl, operation, left, right));
+		};
+
 		var add = function (left, right) {
-			return $http.get(calculatorApiUrl + "/add/" + left + "/" + right + "/");
+			return _exec('add', left, right);
+		};
+
+		var subtract = function (left, right) {
+			return _exec('subtract', left, right);
+		};
+
+		var multiply = function (left, right) {
+			return _exec('multiply', left, right);
+		};
+
+		var divide = function (left, right) {
+			return _exec('divide', left, right);
 		};
 
 		return {
-			add: add
+			add: add,
+			subtract: subtract,
+			multiply: multiply,
+			divide: divide
 		};
 	};
 
--- Web/Areas/Calculator/Scripts/HomeController.js
+++ Web/Areas/Calculator/Scripts/HomeController.js
@@ -1,13 +1,63 @@
 ﻿(function (app) {
 
+	var left;
+	var right;
+	var operation;
+
 	var HomeController = function ($scope, calculatorService) {
 
-		$scope.add = function (left, right) {
-			calculatorService.add(left, right)
-				.success(function (result) {
-					$scope.display = result
-				});
+		$scope.display = 0;
+
+		$scope.numClick = function (val) {
+			$scope.display = $scope.display ? $scope.display + String(val) : String(val);
 		}
+
+		$scope.opClick = function (op) {
+			left = $scope.display;
+			$scope.display = 0;
+			operation = op;
+		};
+
+		$scope.clearClick = function () {
+			left = right = operation = null;
+			$scope.display = 0;
+		};
+
+		$scope.execClick = function () {
+			right = $scope.display;
+			if (left && operation && right) {
+				switch (operation) {
+					case '/':
+						calculatorService.divide(left, right)
+							.success(function (result) {
+								$scope.display = result;
+							});
+						break;
+					case '*':
+						calculatorService.multiply(left, right)
+							.success(function (result) {
+								$scope.display = result;
+							});
+						break;
+					case '-':
+						calculatorService.subtract(left, right)
+							.success(function (result) {
+								$scope.display = result;
+							});
+						break;
+					case '+':
+						calculatorService.add(left, right)
+							.success(function (result) {
+								$scope.display = result;
+							});
+						break;
+					default:
+						$scope.display = 'ERR';
+				}
+			} else {
+				$scope.display = 'ERR';
+			}
+		};
 
 	};
 
